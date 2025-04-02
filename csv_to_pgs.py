13f6b0052619d1369d102b35e16b196e1bf7157a
aadit11 <aadit.kshirsagar@gmail.com>
2025-04-02 08:33:32 +0530

    added file

---
+++ csv_to_pgs.py
@@ -0,0 +1,58 @@
+import psycopg2
+import csv
+import pandas as pd
+
+def infer_data_type(column_name, sample_value):
+    if pd.api.types.is_integer_dtype(sample_value):
+        return 'NUMERIC'
+    elif pd.api.types.is_float_dtype(sample_value):
+        return 'NUMERIC'
+    elif pd.api.types.is_datetime64_any_dtype(sample_value):
+        return 'DATE'
+    elif 'time' in column_name.lower() and ":" in str(sample_value):  
+        return 'TIME'
+    else:
+        return 'TEXT'
+
+# Database connection parameters
+db_params = {
+    'dbname': 'testdb',
+    'user': 'postgres',
+    'password': 'aadit@11',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(**db_params)
+cur = conn.cursor()
+
+# Read the first row of the CSV to get column names
+# csv_path = 'ISO_DB_FILE_v2_updated_v16.csv'
+csv_path = 'local.csv'
+first_row = pd.read_csv(csv_path, nrows=1).iloc[0]
+columns = first_row.index.tolist()
+
+# Dynamically generate the CREATE TABLE query with all columns as TEXT type
+table_creation_query = "CREATE TABLE digipay_temp_table5 (" + ", ".join([f'"{col}" TEXT' for col in columns]) + ");"
+cur.execute(table_creation_query)
+
+# Load data from CSV into the table
+copy_sql = '''
+COPY digipay_temp_table5 FROM stdin WITH CSV HEADER
+DELIMITER as ','
+'''
+with open(csv_path, 'r') as f:
+    cur.copy_expert(sql=copy_sql, file=f)
+conn.commit()
+
+# Create a view based on the table
+view_creation_query = '''
+CREATE VIEW digipay_iso.History_Table AS SELECT * FROM digipay_temp_table5;
+'''
+cur.execute(view_creation_query)
+conn.commit()
+
+# Close the cursor and the connection
+cur.close()
+conn.close()
